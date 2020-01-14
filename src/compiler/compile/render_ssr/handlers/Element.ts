a00f5e895a3ee85e5ea803e6493f286eeb2d3dc3
himynameisdave <dave@fossa.com>
2020-01-13 21:32:28 -0800

    🍂 Make Element work with bind:innerText

--- src/compiler/compile/render_ssr/handlers/Element.ts
+++ src/compiler/compile/render_ssr/handlers/Element.ts
@@ -1,7 +1,9 @@
 import { is_void } from '../../../../shared/utils/names';
 import { get_attribute_expression, get_attribute_value, get_class_attribute_value } from './shared/get_attribute_value';
 import { boolean_attributes } from '../../../../shared/boolean_attributes';
+import isContenteditable, { isBindingContenteditable } from '../../utils/contenteditable';
 import Renderer, { RenderOptions } from '../Renderer';
+import Binding from '../../nodes/Binding';
 import Element from '../../nodes/Element';
 import { p, x } from 'code-red';
 import Expression from '../../nodes/shared/Expression';
@@ -18,11 +20,7 @@ export default function (node: Element, renderer: Renderer, options: RenderOptio
 	// awkward special case
 	let node_contents;
 
-	const contenteditable = (
-		node.name !== 'textarea' &&
-		node.name !== 'input' &&
-		node.attributes.some((attribute) => attribute.name === 'contenteditable')
-	);
+	const contenteditable = isContenteditable(node);
 
 	if (node.is_dynamic_element) {
 		renderer.push();
@@ -125,7 +123,7 @@ export default function (node: Element, renderer: Renderer, options: RenderOptio
 		}
 	}
 
-	node.bindings.forEach(binding => {
+	node.bindings.forEach((binding: Binding) => {
 		const { name, expression } = binding;
 
 		if (binding.is_readonly) {
@@ -141,7 +139,8 @@ export default function (node: Element, renderer: Renderer, options: RenderOptio
 				const condition = type === 'checkbox' ? x`~${bound}.indexOf(${value})` : x`${value} === ${bound}`;
 				renderer.add_expression(x`${condition} ? @add_attribute("checked", true, 1) : ""`);
 			}
-		} else if (contenteditable && (name === 'textContent' || name === 'innerHTML')) {
+			// TODO server-render group bindings
+		} else if (contenteditable && isBindingContenteditable(binding)) {
 			node_contents = expression.node;
 
 			// TODO where was this used?
